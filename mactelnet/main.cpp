bc70d8d14225e4a05f9b549c658eedcb6d19eb76
Wichert Akkerman <wichert@wiggy.net>
2016-01-23 22:58:09 +0100

    Initial Commit

---
+++ mactelnet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mactelnet
+//
+//  Created by Wichert Akkerman on 23/01/16.
+//  Copyright © 2016 Wichert Akkerman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
